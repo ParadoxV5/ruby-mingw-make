ecac8135a5c610a88705b619b07a1a977459277c
ParadoxV5 <paradox.ver5@gmail.com>
2024-02-06 16:45:04 -0700

    Add env var `MAKE = mingw32-make`
    & move env var configs to its own file

---
+++ lib/mingw-make.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Set up `RUBYOPT=-I/absolute/path/to/lib/mingw-make/` for subprocesses
+ENV['RUBYOPT'] = "#{ENV['RUBYOPT']} -I#{File.join __dir__, 'mingw-make'}"
+ENV['MAKE'] ||= 'mingw32-make'
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -17,7 +17,4 @@ else
   end
 end
 
-# Set up `RUBYOPT=-I/absolute/path/to/lib/mingw-make/` for subprocesses
-Gem.pre_install do
-  ENV['RUBYOPT'] = "#{ENV['RUBYOPT']} -I#{File.join __dir__, 'mingw-make'}"
-end
+Gem.pre_install { require 'mingw-make' }
