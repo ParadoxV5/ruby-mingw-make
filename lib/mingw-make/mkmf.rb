371c9a1a9d7d33f1e51ee2e28d7523d36a6e6705
ParadoxV5 <paradox.ver5@gmail.com>
2024-02-06 15:11:30 -0700

    Activate `ruby -run` fallbacks

--- lib/mingw-make/mkmf.rb
+++ lib/mingw-make/mkmf.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 require 'rbconfig'
+
+# Get `mkmf` to fall back to `ruby -run` 
+%w[
+  RM
+  RMALL
+  RMDIRS
+  MAKEDIRS
+  INSTALL
+  INSTALL_PROG
+  INSTALL_DATA
+  CP
+].each do|key|
+  RbConfig::CONFIG.delete key
+  RbConfig::MAKEFILE_CONFIG.delete key
+end
+
 require File.join RbConfig::CONFIG['rubylibdir'], 'mkmf'
 
 module MakeMakefile
